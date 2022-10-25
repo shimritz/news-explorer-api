8477e0ad26a37cd1859d633f4b7ef2561a62b5a9
Shimrit breef Ziskand <shimrit021@gmail.com>
2022-10-25 13:02:51 +0100

    create user scema

---
+++ models/user.js
@@ -0,0 +1,29 @@
+const userSchema = new mongoose.Schema(
+  {
+  
+     email: {
+      type: String,
+      required: [true, 'The "email" field must be filled in'],
+      unique: true,
+      validate: {
+        validator: (v) => validator.isEmail(v),
+        message: 'email is invalid',
+      },
+    },
+    password: {
+      type: String,
+      required: [true, 'The "password" field must be filled in'],
+      select: false,
+    },
+
+      name: {
+      type: String,
+      default: 'Damien',
+      required: [true, 'The "name" field must be filled in'],
+      minlength: [2, 'The minimum length of the "name" field is 2'],
+      maxlength: [30, 'The maximum length of the "name" field is 30'],
+    },
+
+  },
+  { versionKey: false }
+);
