63e92b4865e0c0020ed496567cb10b9dc3ea5568
shimritz <shimrit021@gmail.com>
2023-07-16 21:13:23 +0300

    Update config.js with correct db name

--- utils/config.js
+++ utils/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 // module.exports.JWT_SECRET = process.env.JWT_SECRET || "default_value_dfljhbdfk";
 // module.exports.JWT_SECRET = "dnjuhncj12";
 const { JWT_SECRET = 'mysecret' } = process.env;
-const { DB_ADDRESS = 'mongodb://localhost:27017/test' } = process.env;
+const { DB_ADDRESS = 'mongodb://localhost:27017/newsdb' } = process.env;
 module.exports = {
   JWT_SECRET,
   DB_ADDRESS,
